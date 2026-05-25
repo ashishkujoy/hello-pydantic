a1d1a94338f2b9e062ad1199e4829a10d370f2bd
Ashish <ashishkujoy@gmail.com>
2026-05-25 22:12:18 +0530

    Adds thinking and websearch capabilities

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
 import asyncio
 
 from pydantic_ai import Agent
+from pydantic_ai.capabilities import Thinking, WebSearch
+
 
 async def main():
-    agent = Agent(model="anthropic:claude-sonnet-4-6")
+    agent = Agent(
+        model="anthropic:claude-sonnet-4-6",
+        instructions="Be precise and always reply in one sentence only.",
+        capabilities=[Thinking(), WebSearch(local="duckduckgo")]
+    )
 
-    res = await agent.run("Hello how are you")
+    res = await agent.run("What was the mass of the largest meteorite found this year?")
     print(res.output)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
